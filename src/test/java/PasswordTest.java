d762d06ed708897effb45ad20479b1f07d0aaae4
183tm <17thaicolony@gmail.com>
2022-11-04 15:30:49 +0300

    Задание 9

---
+++ src/test/java/PasswordTest.java
@@ -0,0 +1,77 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PasswordTest {
+
+    @Test
+    public void passTest() {
+
+        ArrayList<String> passwords = new ArrayList<>();
+        passwords.add("123456");
+        passwords.add("123456789");
+        passwords.add("qwerty");
+        passwords.add("password");
+        passwords.add("1234567");
+        passwords.add("12345678");
+        passwords.add("12345");
+        passwords.add("iloveyou");
+        passwords.add("111111");
+        passwords.add("123123");
+        passwords.add("abc123");
+        passwords.add("qwerty123");
+        passwords.add("1q2w3e4r");
+        passwords.add("admin");
+        passwords.add("qwertyuiop");
+        passwords.add("654321");
+        passwords.add("555555");
+        passwords.add("lovely");
+        passwords.add("7777777");
+        passwords.add("welcome");
+        passwords.add("888888");
+        passwords.add("princess");
+        passwords.add("dragon");
+        passwords.add("password1");
+        passwords.add("123qwe");
+
+
+        for (String pass : passwords) {
+
+            Map<String, String> params = new HashMap<>();
+            params.put("login", "super_admin");
+            params.put("password", pass);
+            Response getSecretPass = RestAssured
+                    .given()
+                    .body(params)
+                    .post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework")
+                    .andReturn();
+            String cookie = getSecretPass.getCookie("auth_cookie");
+
+            Map<String, String> cookies = new HashMap<>();
+            cookies.put("auth_cookie", cookie);
+
+            Response checkCookie = RestAssured
+                    .given()
+                    .body(params)
+                    .cookies(cookies)
+                    .post("https://playground.learnqa.ru/ajax/api/check_auth_cookie")
+                    .andReturn();
+            String answer = checkCookie.asString();
+
+            if (!answer.equals("You are NOT authorized")) {
+
+                System.out.println(answer);
+                System.out.println(pass);
+                break;
+            }
+        }
+
+    }
+
+}
+
+
